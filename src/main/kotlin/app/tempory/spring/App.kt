29dee1a8d1131d75c107a94af7a4d51d905c030f
andrethiele <thielea@th-brandenburg.de>
2021-07-27 09:12:21 +0200

    hardcode api key placeholder

--- src/main/kotlin/app/tempory/spring/App.kt
+++ src/main/kotlin/app/tempory/spring/App.kt
@@ -1,7 +1,6 @@
 package app.tempory.spring
 
 import com.google.maps.GeoApiContext
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -9,13 +8,10 @@ import org.springframework.context.annotation.Bean
 @SpringBootApplication
 class App {
 
-    @Value("\${GOOGLE_MAPS_SERVICES_API_KEY}")
-    lateinit var apiKey: String
-
     @Bean
     fun provideGeoApiContext(): GeoApiContext {
         return GeoApiContext.Builder()
-            .apiKey(apiKey)
+            .apiKey("YOUR_API_KEY")
             .build()
     }
 }
